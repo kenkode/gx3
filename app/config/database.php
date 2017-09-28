b063c7c06bf0f6bfb51184743d44d79696333d45
vomwaka <vomwaka12@gmail.com>
2017-09-28 12:01:29 +0300

    new

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'gx3',
+			'database'  => 'gas3',
 			'username'  => 'root',
 			'password'  => '',
 			'port' => '3306',
